786866a99c5db24005bc15f28685414b386fc6c7
gi.ran <domotoweilan@gmail.com>
2023-04-03 10:45:16 +0900

    idで検査可能

--- src/main/java/com/site/controller/ItemListController.java
+++ src/main/java/com/site/controller/ItemListController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.site.enitity.SiteItem;
@@ -32,6 +33,23 @@ public class ItemListController {
 			logger.error("Get item list failed.");
 			throw e;
 		}
+	}
+	@GetMapping("/getList/{id}")
+	private JSONResult getDetail(@PathVariable("id") long id) {
+		try {
+			SiteItem siteItem = service.getDetail(id);
+			if(siteItem != null){
+				logger.info("Get item list {} records");
+			}
+			return JSONResult.ok(siteItem);
+		} catch (Exception e) {
+			logger.error("Get item list failed.");
+			throw e;
+			// TODO: handle exception
+		}
+		
+		
+		
 	}
 
 }
